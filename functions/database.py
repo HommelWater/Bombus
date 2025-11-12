bba0a9db40f68069ca3e26017f399196503b99bd
smonnnn <slooij1@gmail.com>
2025-11-12 10:05:19 +0100

    debugging

--- functions/database.py
+++ functions/database.py
@@ -136,8 +136,17 @@ async def add_push_subscription(cursor, user_id, subscription):
         import json
         subscription = json.dumps(subscription)
 
-    await cursor.execute('INSERT INTO push_subscriptions (user_id, endpoint, subscription_json) VALUES (?, ?, ?)', (user_id, endpoint, subscription))
-    return cursor.lastrowid
+    await cursor.execute(
+        '''
+        INSERT INTO push_subscriptions (user_id, endpoint, subscription_json)
+        VALUES (?, ?, ?)
+        ON CONFLICT(endpoint) DO UPDATE SET
+            user_id = excluded.user_id,
+            subscription_json = excluded.subscription_json
+        ''',
+        (user_id, endpoint, subscription)
+    )
+    return endpoint
 
 @async_with_db(commit=True)
 async def remove_push_subscription(cursor, user_id, endpoint):
