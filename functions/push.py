3116100279d706307fac2a4de5c95fc84f34f020
smonnnn <slooij1@gmail.com>
2025-11-12 10:23:42 +0100

    fix get push subscriptions

--- functions/push.py
+++ functions/push.py
@@ -38,6 +38,7 @@ async def unsubscribe(sender_user_id, subscription):
     await send(sender_user_id, {"type":"success", "data":{"notification":"Successfully unsubscribed to push notifications."}})
 
 async def send_notifications(user_ids, message):
+    print(message)
     subscriptions = await database.get_push_subscriptions(user_ids)
     subscriptions = [json.loads(sub) for sub in subscriptions]
     if not subscriptions: return
