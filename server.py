56c84b659dd7a516b920dbf2066d939ecb2bf9a7
smonnnn <slooij1@gmail.com>
2025-11-19 14:57:40 +0100

    Finally

--- server.py
+++ server.py
@@ -105,7 +105,7 @@ async def endpoint(websocket: WebSocket):
     try:
         while True:
             data = await websocket.receive_json()
-            if not data["type"] == "file_upload": print(data)
+            if not data["type"] == "file_upload": print(data, flush=True)
 
             message_type = data.get("type")
             if not message_type or message_type not in request_map:
